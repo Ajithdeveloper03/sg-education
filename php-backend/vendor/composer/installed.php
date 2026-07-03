9a7719df75f469c0e08cabf371d73f67878b9779
Ajithdeveloper03 <ajithdeveloper03@gmail.com>
2026-07-03 15:13:35 +0530

    feat: commit PHPMailer vendor directory so no composer install needed on server

---
+++ php-backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sg-education/php-backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c29f59f821c263db8bca6dc4ca8670d37074a3bc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sg-education/php-backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c29f59f821c263db8bca6dc4ca8670d37074a3bc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
